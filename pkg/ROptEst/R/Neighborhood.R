943efbf2cf23c109183507c539bf22e8f0d9f873
stamats <stamats@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-14 08:53:26 +0000

    classes and functions/methods moved to distrMod and RobAStBase, respectively
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robast@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ROptEst/R/Neighborhood.R
+++
@@ -1,3 +0,0 @@
-## access method
-setMethod("type", "Neighborhood", function(object) object@type)
-setMethod("radius", "Neighborhood", function(object) object@radius)
--- pkg/ROptEst/R/UncondNeighborhood.R
+++
@@ -1,9 +0,0 @@
-## generating function
-ContNeighborhood <- function(radius = 0){ 
-    new("ContNeighborhood", radius = radius) 
-}
-
-## generating function
-TotalVarNeighborhood <- function(radius = 0){ 
-    new("TotalVarNeighborhood", radius = radius) 
-}
