2b9f546dd3036c3bc616bf77d6538a97b9ce81ff
stamats <stamats@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-18 07:15:54 +0000

    added require for "RobAStBase"
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robast@35 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ROptEst/R/AllClass.R
+++ pkg/ROptEst/R/AllClass.R
@@ -2,8 +2,9 @@
     require("methods", character = TRUE, quietly = TRUE)
     require("distr", character = TRUE, quietly = TRUE)
     require("distrEx", character = TRUE, quietly = TRUE)
-    require("distrMod", character = TRUE, quietly = TRUE)
     require("RandVar", character = TRUE, quietly = TRUE)
+    require("distrMod", character = TRUE, quietly = TRUE)
+    require("RobAStBase", character = TRUE, quietly = TRUE)
 }
 
 
