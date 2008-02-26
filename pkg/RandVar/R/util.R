57932803e8b0e9d751d539353fecf99a01e10176
ruckdeschel <ruckdeschel@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-26 22:56:45 +0000

    small change for consistency with method "Math" for ContinuousDistribution.R from package distr
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robast@76 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RandVar/R/util.R
+++ pkg/RandVar/R/util.R
@@ -3,7 +3,5 @@
 { if (is(try(return(f(distr)), silent = TRUE),
          "try-error")){
   rl <- function(n) { xr <- r(distr)(n); f(xr) }
-  dr <- RtoDPQ(r = rl)
-  return(new("AbscontDistribution", d = dr$dfun, 
-       r = rl, p = dr$pfun, q = dr$qfun, .withSim = TRUE))}
-}       
+  return(new("AbscontDistribution", r = rl, .withArith = TRUE, .withSim = TRUE))}
+}         
