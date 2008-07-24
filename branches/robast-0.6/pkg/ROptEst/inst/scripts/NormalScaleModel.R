aece8b9dba65ebc6e4d33617141d133d3238d49a
stamats <stamats@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-07-24 10:21:49 +0000

    no problems with new implementation
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robast@118 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/robast-0.6/pkg/ROptEst/inst/scripts/NormalScaleModel.R
+++ branches/robast-0.6/pkg/ROptEst/inst/scripts/NormalScaleModel.R
@@ -3,25 +3,25 @@
 ###############################################################################
 require(ROptEst)
 
-## generates Normal Scale Family with scale = 1
-N0 <- NormScaleFamily(mean=0, sd=1) 
+## generates Normal Scale Family with scale = 3
+N0 <- NormScaleFamily(mean=2, sd=3) 
 N0        # show G0
-plot(N0)  # plot of Norm(mean = 0, sd = 1) and L_2 derivative
+plot(N0)  # plot of Norm(mean = 2, sd = 3) and L_2 derivative
 checkL2deriv(N0)
 
-# classical optimal IC
+## classical optimal IC
 N0.IC0 <- optIC(model = N0, risk = asCov())
 N0.IC0       # show IC
 plot(N0.IC0) # plot IC
 checkIC(N0.IC0)
 Risks(N0.IC0)
 
-# L_2 family + infinitesimal neighborhood
+## L_2 family + infinitesimal neighborhood
 N0.Rob1 <- InfRobModel(center = N0, neighbor = ContNeighborhood(radius = 0.5))
 N0.Rob1     # show N0.Rob1
 N0.Rob2 <- InfRobModel(center = N0, neighbor = TotalVarNeighborhood(radius = 0.5))
 
-# MSE solution
+## MSE solution
 (N0.IC1 <- optIC(model=N0.Rob1, risk=asMSE()))
 checkIC(N0.IC1)
 Risks(N0.IC1)
@@ -32,7 +32,7 @@ checkIC(N0.IC2)
 Risks(N0.IC2)
 plot(N0.IC2)
 
-# lower case solutions
+## lower case solutions
 (N0.IC3 <- optIC(model=N0.Rob1, risk=asBias()))
 checkIC(N0.IC3)
 Risks(N0.IC3)
@@ -42,7 +42,7 @@ checkIC(N0.IC4)
 Risks(N0.IC4)
 plot(N0.IC4)
 
-# Hampel solution
+## Hampel solution
 (N0.IC5 <- optIC(model=N0.Rob1, risk=asHampel(bound=clip(N0.IC1))))
 checkIC(N0.IC5)
 Risks(N0.IC5)
@@ -52,7 +52,7 @@ checkIC(N0.IC6)
 Risks(N0.IC6)
 plot(N0.IC6)
 
-# radius minimax IC
+## radius minimax IC
 (N0.IC7 <- radiusMinimaxIC(L2Fam=N0, neighbor=ContNeighborhood(), 
                 risk=asMSE(), loRad=0, upRad=Inf))
 checkIC(N0.IC7)
@@ -64,8 +64,8 @@ checkIC(N0.IC8)
 Risks(N0.IC8)
 plot(N0.IC8)
 
-# least favorable radius
-# (may take quite some time!)
+## least favorable radius
+## (may take quite some time!)
 (N0.r.rho1 <- leastFavorableRadius(L2Fam=N0, neighbor=ContNeighborhood(),
                     risk=asMSE(), rho=0.5))
 (N0.r.rho2 <- leastFavorableRadius(L2Fam=N0, neighbor=TotalVarNeighborhood(),
