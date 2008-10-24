ba356f27fb0641f256870d72ddffa0b38aafd546
stamats <stamats@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-10-24 09:23:11 +0000

    some minor modifications ... seems to work now
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robast@182 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/ROptEst/inst/scripts/LognormalAndNormalModel.R
+++ pkg/ROptEst/inst/scripts/LognormalAndNormalModel.R
@@ -3,10 +3,10 @@
 ###############################################################################
 require(ROptEst)
 
-## generates Lognormal Scale Family with rate = 1
+## generates Lognormal Scale Family with meanlog = 0, sdlog = 1
 LN1 <- LnormScaleFamily() 
 LN1        # show LN1
-plot(LN1)  # plot of Exp(rate = 1) and L_2 derivative
+plot(LN1)  # plot of Lnorm() and L_2 derivative
 checkL2deriv(LN1)
 
 ## generates Normal Location Family with mean = 0
@@ -153,3 +153,22 @@ plot(N0.IC8)
                     risk=asMSE(), rho=1/3))
 (N0.r.rho2 <- leastFavorableRadius(L2Fam=N0, neighbor=TotalVarNeighborhood(),
                     risk=asMSE(), rho=1/3))
+
+## For estimation use function roptest
+ind <- rbinom(1e2, size=1, prob=0.05) 
+x <- rnorm(1e2, mean=(1-ind)+ind*9)
+y <- exp(x)
+
+## 1-step: contamination known
+est1 <- roptest(x, eps = 0.05, L2Fam = NormLocationFamily())
+est2 <- roptest(y, eps = 0.05, L2Fam = LnormScaleFamily())
+
+## k-step: contamination known
+est3 <- roptest(x, eps = 0.05, L2Fam = NormLocationFamily(), steps = 3)
+est4 <- roptest(y, eps = 0.05, L2Fam = LnormScaleFamily(), steps = 3)
+
+## comparison
+estimate(est1)
+log(estimate(est2))
+estimate(est3)
+log(estimate(est4))
