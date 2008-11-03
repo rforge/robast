d962417b60598873ab2d0636ed515ec1907f442f
stamats <stamats@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-03 06:32:15 +0000

    just formating ....
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robast@203 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RandVar/R/util.R
+++ pkg/RandVar/R/util.R
@@ -1,9 +1,12 @@
 ## small util if imageDistr fails
-.getImageDistr <- function(f, distr)
-{ if (is(distr, "DiscreteDistribution"))
-      return(DiscreteDistribution(prob=d(distr)(support(distr)), supp=f(support(distr))))
-  if (is(try(return(f(distr)), silent = TRUE),
-         "try-error")){
-  rl <- function(n) { xr <- r(distr)(n); f(xr) }
-  return(AbscontDistribution( r = rl, .withArith = TRUE, .withSim = TRUE))}
-}         
+.getImageDistr <- function(f, distr){ 
+    if (is(distr, "DiscreteDistribution"))
+        return(DiscreteDistribution(prob=d(distr)(support(distr)), supp=f(support(distr))))
+    if(is(try(return(f(distr)), silent = TRUE), "try-error")){
+        rl <- function(n){ 
+            xr <- r(distr)(n) 
+            f(xr) 
+        }
+        return(AbscontDistribution(r = rl, .withArith = TRUE, .withSim = TRUE))
+    }
+}
