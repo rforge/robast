67f21a9a86fa14a00cde1315fce3a0b5ecf47bc4
stamats <stamats@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-07-15 10:06:41 +0000

    implementation of oneStepEstimator simplified, no longer dispatching on argument "start".
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robast@323 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/robast-0.7/pkg/RobAStBase/R/oneStepEstimator.R
+++ branches/robast-0.7/pkg/RobAStBase/R/oneStepEstimator.R
@@ -3,152 +3,16 @@
 ###############################################################################
 setMethod("oneStepEstimator", signature(x = "numeric", 
                                         IC = "InfluenceCurve",
-                                        start = "numeric"),
+                                        start = "ANY"),
     function(x, IC, start, useLast = getRobAStBaseOption("kStepUseLast")){
         es.call <- match.call()
         es.call[[1]] <- as.name("oneStepEstimator")
         nrvalues <- dimension(IC@Curve)
-        if(is.list(start)) start <- unlist(start)
-        if(nrvalues != length(start))
-            stop("dimension of 'start' != dimension of 'Curve'")
-
-        res <- start + rowMeans(evalIC(IC, as.matrix(x)), na.rm = TRUE)
-
-        if(is(IC, "IC")){
-            L2Fam <- eval(CallL2Fam(IC))
-            Infos <- matrix(c("oneStepEstimator", 
-                            paste("1-step estimate for", name(L2Fam))),
-                            ncol = 2)
-            colnames(Infos) <- c("method", "message")
-            if(is(L2Fam, "L2GroupParamFamily")) useLast <- TRUE
-            if(useLast && !is(modifyIC(IC), "NULL") ){
-                newParam <- param(L2Fam)
-                main(newParam)[] <- res
-                newL2Fam <- modifyModel(L2Fam, newParam)
-                IC <- modifyIC(IC)(newL2Fam, IC)
-                Infos <- rbind(Infos, c("oneStepEstimator", 
-                                        "computation of IC, asvar and asbias via useLast = TRUE"))
-            }else{
-                if(useLast && is(modifyIC(IC), "NULL")){
-                    warning("'useLast = TRUE' only possible if slot 'modifyIC' of 'IC'
-                             is filled with some function!")
-                    Infos <- rbind(Infos, c("oneStepEstimator", 
-                                            "slot 'modifyIC' of 'IC' was not filled!"))
-                }
-                Infos <- rbind(Infos, c("oneStepEstimator", 
-                                        "computation of IC, asvar and asbias via useLast = FALSE"))
-            }
-            if("asCov" %in% names(Risks(IC)))
-                if(is.matrix(Risks(IC)$asCov) || length(Risks(IC)$asCov) == 1)
-                    asVar <- Risks(IC)$asCov
-                else
-                    asVar <- Risks(IC)$asCov$value
-            else
-                asVar <- getRiskIC(IC, risk = asCov())$asCov$value
-
-            if("asBias" %in% names(Risks(IC))){
-                if(length(Risks(IC)$asBias) == 1)
-                    asBias <- neighborRadius(IC)*Risks(IC)$asBias
-                else
-                    asBias <- neighborRadius(IC)*Risks(IC)$asBias$value
-            }else{
-                if(is(IC, "HampIC")){
-                    r <- neighborRadius(IC)
-                    asBias <- r*getRiskIC(IC, risk = asBias(), neighbor = neighbor(IC))$asBias$value
-                }else{
-                    asBias <- NULL
-                }
-            }
+        if(is(start, "Estimate")){ 
+            start0 <- estimate(start)
         }else{
-            Infos <- matrix(c("oneStepEstimator", "1-step estimate"), ncol = 2)
-            colnames(Infos) <- c("method", "message")
-            asVar <- NULL
-            asBias <- NULL
+            start0 <- start
         }
-
-        new("kStepEstimate", name = "1-step estimate", estimate = res, 
-            estimate.call = es.call, samplesize = length(x), asvar = asVar, 
-            asbias = asBias, pIC = IC, steps = 1L, Infos = Infos)
-    })
-setMethod("oneStepEstimator", signature(x = "matrix", 
-                                        IC = "InfluenceCurve",
-                                        start = "numeric"),
-    function(x, IC, start, useLast = getRobAStBaseOption("kStepUseLast")){
-        es.call <- match.call()
-        es.call[[1]] <- as.name("oneStepEstimator")
-        nrvalues <- dimension(IC@Curve)
-        if(is.list(start)) start <- unlist(start)
-        if(nrvalues != length(start))
-            stop("dimension of 'start' != dimension of 'Curve'")
-        if(ncol(x) != IC@Curve[[1]]@Domain@dimension)
-            stop("'x' has wrong dimension")
-
-        res <- start + rowMeans(evalIC(IC, x), na.rm = TRUE)
-
-        if(is(IC, "IC")){
-            L2Fam <- eval(CallL2Fam(IC))
-            Infos <- matrix(c("oneStepEstimator", 
-                            paste("1-step estimate for", name(L2Fam))),
-                            ncol = 2)
-            colnames(Infos) <- c("method", "message")
-            if(is(L2Fam, "L2GroupParamFamily")) useLast <- TRUE
-            if(useLast && !is(modifyIC(IC), "NULL") ){
-                newParam <- param(L2Fam)
-                main(newParam)[] <- res
-                newL2Fam <- modifyModel(L2Fam, newParam)
-                IC <- modifyIC(IC)(newL2Fam, IC)
-                Infos <- rbind(Infos, c("oneStepEstimator", 
-                                        "computation of IC, asvar and asbias via useLast = TRUE"))
-            }else{
-                if(useLast && is(modifyIC(IC), "NULL")){
-                    warning("'useLast = TRUE' only possible if slot 'modifyIC' of 'IC'
-                             is filled with some function!")
-                    Infos <- rbind(Infos, c("oneStepEstimator", 
-                                            "slot 'modifyIC' of 'IC' was not filled!"))
-                }
-                Infos <- rbind(Infos, c("oneStepEstimator", 
-                                        "computation of IC, asvar and asbias via useLast = FALSE"))
-            }
-            if("asCov" %in% names(Risks(IC)))
-                if(is.matrix(Risks(IC)$asCov) || length(Risks(IC)$asCov) == 1)
-                    asVar <- Risks(IC)$asCov
-                else
-                    asVar <- Risks(IC)$asCov$value
-            else
-                asVar <- getRiskIC(IC, risk = asCov())$asCov$value
-
-            if("asBias" %in% names(Risks(IC))){
-                if(length(Risks(IC)$asBias) == 1)
-                    asBias <- neighborRadius(IC)*Risks(IC)$asBias
-                else
-                    asBias <- neighborRadius(IC)*Risks(IC)$asBias$value
-            }else{
-                if(is(IC, "HampIC")){
-                    r <- neighborRadius(IC)
-                    asBias <- r*getRiskIC(IC, risk = asBias(), neighbor = neighbor(IC))$asBias$value
-                }else{
-                    asBias <- NULL
-                }
-            }
-        }else{
-            Infos <- matrix(c("oneStepEstimator", "1-step estimate"), ncol = 2)
-            colnames(Infos) <- c("method", "message")
-            asVar <- NULL
-            asBias <- NULL
-        }
-
-        new("kStepEstimate", name = "1-step estimate", estimate = res, 
-            estimate.call = es.call, samplesize = ncol(x), asvar = asVar, 
-            asbias = asBias, pIC = IC, steps = 1L, Infos = Infos)
-    })
-setMethod("oneStepEstimator", signature(x = "numeric", 
-                                        IC = "InfluenceCurve",
-                                        start = "Estimate"),
-    function(x, IC, start, useLast = getRobAStBaseOption("kStepUseLast")){
-        es.call <- match.call()
-        es.call[[1]] <- as.name("oneStepEstimator")
-        nrvalues <- dimension(IC@Curve)
-        start0 <- estimate(start)
         if(is.list(start0)) start0 <- unlist(start0)
         if(nrvalues != length(start0))
             stop("dimension of slot 'estimate' of 'start' != dimension of 'Curve'")
@@ -168,7 +32,7 @@ setMethod("oneStepEstimator", signature(x = "numeric",
                 newL2Fam <- modifyModel(L2Fam, newParam)
                 IC <- modifyIC(IC)(newL2Fam, IC)
                 Infos <- rbind(Infos, c("oneStepEstimator", 
-                                        "computation of IC, asvar and asbias via useLast = TRUE"))
+                                        "computation of IC, asVar and asBias via useLast = TRUE"))
             }else{
                 if(useLast && is(modifyIC(IC), "NULL")){
                     warning("'useLast = TRUE' only possible if slot 'modifyIC' of 'IC' 
@@ -177,7 +41,7 @@ setMethod("oneStepEstimator", signature(x = "numeric",
                                             "slot 'modifyIC' of 'IC' was not filled!"))
                 }
                 Infos <- rbind(Infos, c("oneStepEstimator", 
-                                        "computation of IC, asvar and asbias via useLast = FALSE"))
+                                        "computation of IC, asVar and asBias via useLast = FALSE"))
             }
             if("asCov" %in% names(Risks(IC)))
                 if(is.matrix(Risks(IC)$asCov) || length(Risks(IC)$asCov) == 1)
@@ -213,12 +77,16 @@ setMethod("oneStepEstimator", signature(x = "numeric",
     })
 setMethod("oneStepEstimator", signature(x = "matrix", 
                                         IC = "InfluenceCurve",
-                                        start = "Estimate"),
+                                        start = "ANY"),
     function(x, IC, start, useLast = getRobAStBaseOption("kStepUseLast")){
         es.call <- match.call()
         es.call[[1]] <- as.name("oneStepEstimator")
         nrvalues <- dimension(IC@Curve)
-        start0 <- estimate(start)
+        if(is(start, "Estimate")){ 
+            start0 <- estimate(start)
+        }else{
+            start0 <- start
+        }
         if(is.list(start0)) start0 <- unlist(start0)
         if(nrvalues != length(start0))
             stop("dimension of slot 'estimate' of 'start' != dimension of 'Curve'")
@@ -240,7 +108,7 @@ setMethod("oneStepEstimator", signature(x = "matrix",
                 newL2Fam <- modifyModel(L2Fam, newParam)
                 IC <- modifyIC(IC)(newL2Fam, IC)
                 Infos <- rbind(Infos, c("oneStepEstimator", 
-                                        "computation of IC, asvar and asbias via useLast = TRUE"))
+                                        "computation of IC, asVar and asBias via useLast = TRUE"))
             }else{
                 if(useLast && is(modifyIC(IC), "NULL")){
                     warning("'useLast = TRUE' only possible if slot 'modifyIC' of 'IC'
@@ -249,7 +117,7 @@ setMethod("oneStepEstimator", signature(x = "matrix",
                                             "slot 'modifyIC' of 'IC' was not filled!"))
                 }
                 Infos <- rbind(Infos, c("oneStepEstimator", 
-                                        "computation of IC, asvar and asbias via useLast = FALSE"))
+                                        "computation of IC, asVar and asBias via useLast = FALSE"))
             }
             if("asCov" %in% names(Risks(IC)))
                 if(is.matrix(Risks(IC)$asCov) || length(Risks(IC)$asCov) == 1)
