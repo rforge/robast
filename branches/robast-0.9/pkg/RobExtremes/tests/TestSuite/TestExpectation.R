2c5a92bc6c63db2b9aacff152f8282d44e3eef55
horbenko <horbenko@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-06 09:45:54 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robast@506 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/robast-0.9/pkg/RobExtremes/tests/TestSuite/TestExpectation.R
@@ -0,0 +1,75 @@
+##########################################
+##                                      ## 
+##        Tests for Expectation.R       ##
+##                                      ##
+##########################################
+
+
+ .setUp()
+ {
+  test.expectationPareto = function(){
+   X = Pareto(shape=1,Min=1)
+   expectation.Pareto = E(X)
+   checkEquals(expectation.Pareto, Inf)
+   }
+  }
+
+ .tearDown()
+ {
+  }
+
+#  
+# 
+# .setUp()
+# {
+# test.checkFunctions1 = function(){
+# checkTrue(1 < 2, "check1") ## passes fine
+# ## checkTrue(1 > 2, "check2") ## appears as failure in the test protocol
+# v <- 1:3
+# w <- 1:3
+# checkEquals(v, w) ## passes fine
+# names(v) <- c("A", "B", "C")
+# ## checkEquals(v, w) ## fails because v and w have different names
+# checkEqualsNumeric(v, w) ## passes fine because names are ignored
+# x <- rep(1:12, 2)
+# y <- rep(0:1, 12)
+# res <- list(a=1:3, b=letters, LM=lm(y ~ x))
+# res2 <- list(a=seq(1,3,by=1), b=letters, LM=lm(y ~ x))
+# checkEquals(res, res2) ## passes fine
+# checkIdentical(res, res)
+# checkIdentical(res2, res2)
+# ## checkIdentical(res, res2) ## fails because element ’a’ differs in type
+# }
+# }
+# .tearDown()
+# {}
+# 
+# fun <- function(x) {
+# if(x)
+# {
+# stop("stop conditions signaled")
+# }
+# return()
+# 
+# }
+# 
+# .setUp()
+# {
+# test.checkFunctions2 = function(){
+# checkException(fun(TRUE)) ## passes fine
+# ## checkException(fun(FALSE)) ## failure, because fun raises no error
+# checkException(fun(TRUE), silent=TRUE)
+# ## special constants
+# ## same behaviour as for underlying base functions
+# checkEquals(NA, NA)
+# checkEquals(NaN, NaN)
+# checkEquals(Inf, Inf)
+# checkIdentical(NA, NA)
+# checkIdentical(NaN, NaN)
+# checkIdentical(-Inf, -Inf)
+# ## DEACTIVATED("here one can document on the reason for deactivation")
+# }
+# }
+# 
+# .tearDown()
+# {}
