c903ca3fb31ffd206e1b1caa60bef75602335096
horbenko <horbenko@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-06 08:03:14 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robast@503 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/robast-0.9/pkg/RobExtremes/tests/TestSuite.R
@@ -0,0 +1,25 @@
+#Options: if the exceptions are shown as errors
+## Not run:
+## quiet log output
+ro <- getOption("RUnit")
+ro$silent <- TRUE
+ro$verbose <- 0L
+options("RUnit"=ro)
+## End(Not run)
+
+## run test suite
+myTestSuite <- defineTestSuite("RUnit Example",dirs="./tests",
+testFileRegexp = "^test.+\\R$",testFuncRegexp = "^test.+")
+testResult <- runTestSuite(myTestSuite)
+
+## is test suite valid?
+isValidTestSuite(myTestSuite)
+
+## get all the errors
+#getErrors(myTestSuite)
+
+## prints detailed text protocol
+## to standard out:
+printTextProtocol(testResult, "testResult.txt",showDetails = TRUE)
+printHTMLProtocol(testResult, "testResult.html")
+ 
