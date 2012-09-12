1ca5780a728d82fbc61d33fd4abcaa666ef9acab
stamats <stamats@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-09-12 18:01:54 +0000

    Kurt Hornik (CRAN) reported problems with the approach of using approxfun. Hence, I've implemented a new approach based on approx and saved values.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/robast@519 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RobLox/inst/scripts/LMinterpolation.R
+++ pkg/RobLox/inst/scripts/LMinterpolation.R
@@ -19,70 +19,25 @@ locationScale <- sapply(radius, fun)
 #locationScale <- sapply(radius, rlsOptIC.AL, computeIC = FALSE)
 
 ## location
-A.loc <- unlist(location[1,])
-b.loc <- unlist(location[3,])
-if(getRversion() < "2.16.0"){
-  .getA.loc.old <- approxfun(radius, A.loc, yleft = 1)
-  .getb.loc.old <- approxfun(radius, b.loc, yleft = Inf)
-  .getA.loc.old <- approxfun(radius, A.loc, yleft = 1)
-  .getb.loc.old <- approxfun(radius, b.loc, yleft = Inf)
-}else{
-  .getA.loc.new <- approxfun(radius, A.loc, yleft = 1)
-  .getb.loc.new <- approxfun(radius, b.loc, yleft = Inf)
-  .getA.loc.new <- approxfun(radius, A.loc, yleft = 1)
-  .getb.loc.new <- approxfun(radius, b.loc, yleft = Inf)
-}
+.A.loc <- unlist(location[1,])
+.b.loc <- unlist(location[3,])
 
 ## scale
-A.sc <- unlist(scale[1,])
-a.sc <- unlist(scale[2,])
-b.sc <- unlist(scale[3,])
-if(getRversion() < "2.16.0"){
-  .getA.sc.old <- approxfun(radius, A.sc, yleft = 0.5)
-  .geta.sc.old <- approxfun(radius, a.sc, yleft = 0)
-  .getb.sc.old <- approxfun(radius, b.sc, yleft = Inf)
-}else{
-  .getA.sc.new <- approxfun(radius, A.sc, yleft = 0.5)
-  .geta.sc.new <- approxfun(radius, a.sc, yleft = 0)
-  .getb.sc.new <- approxfun(radius, b.sc, yleft = Inf)
-}
+.A.sc <- unlist(scale[1,])
+.a.sc <- unlist(scale[2,])
+.b.sc <- unlist(scale[3,])
 
 ## location and scale
 n <- length(radius)
-A1.locsc <- unlist(locationScale[1,])[seq(1, 4*n-3, by = 4)]
-A2.locsc <- unlist(locationScale[1,])[seq(4, 4*n, by = 4)]
-a.locsc <- unlist(locationScale[2,])[seq(2, 2*n, by = 2)]
-b.locsc <- unlist(locationScale[3,])
-if(getRversion() < "2.16.0"){
-  .getA1.locsc.old <- approxfun(radius, A1.locsc, yleft = 1)
-  .getA2.locsc.old <- approxfun(radius, A2.locsc, yleft = 0.5)
-  .geta.locsc.old <- approxfun(radius, a.locsc, yleft = 0)
-  .getb.locsc.old <- approxfun(radius, b.locsc, yleft = Inf)
-}else{
-  .getA1.locsc.new <- approxfun(radius, A1.locsc, yleft = 1)
-  .getA2.locsc.new <- approxfun(radius, A2.locsc, yleft = 0.5)
-  .geta.locsc.new <- approxfun(radius, a.locsc, yleft = 0)
-  .getb.locsc.new <- approxfun(radius, b.locsc, yleft = Inf)
-}
+.A1.locsc <- unlist(locationScale[1,])[seq(1, 4*n-3, by = 4)]
+.A2.locsc <- unlist(locationScale[1,])[seq(4, 4*n, by = 4)]
+.a.locsc <- unlist(locationScale[2,])[seq(2, 2*n, by = 2)]
+.b.locsc <- unlist(locationScale[3,])
 
-if(getRversion() < "2.16.0"){
-  save(.getA.loc.old, .getb.loc.old, .getA.sc.old, .geta.sc.old, .getb.sc.old, 
-       .getA1.locsc.old, .getA2.locsc.old, .geta.locsc.old, .getb.locsc.old, 
-       file = "savedataOld.rda")
-}else{
-  save(.getA.loc.new, .getb.loc.new, .getA.sc.new, .geta.sc.new, .getb.sc.new, 
-       .getA1.locsc.new, .getA2.locsc.new, .geta.locsc.new, .getb.locsc.new, 
-       file = "savedataNew.rda")
-}
+.radius.gitter <- radius
 
 ## Saving the results in sysdata.rda
 #load("sysdata.rda")
-#load("savedataOld.rda")
-#load("savedataNew.rda")
-#save(.finiteSampleRadius.loc, .finiteSampleRadius.locsc, .finiteSampleRadius.sc, 
-#     .getA1.locsc.new, .getA1.locsc.old, .getA2.locsc.new, .getA2.locsc.old, 
-#     .getA.loc.new, .getA.loc.old, .geta.locsc.new, .geta.locsc.old, 
-#     .geta.sc.new, .getA.sc.new, .geta.sc.old, .getA.sc.old, 
-#     .getb.loc.new, .getb.loc.old, .getb.locsc.new, .getb.locsc.old, 
-#     .getb.sc.new, .getb.sc.old, file = "sysdata.rda")
-
+save(.radius.gitter, .finiteSampleRadius.loc, .finiteSampleRadius.locsc, .finiteSampleRadius.sc,
+     .A.loc, .b.loc, .A.sc, .a.sc, .b.sc, .A1.locsc, .A2.locsc, .a.locsc, .b.locsc,
+     file = "sysdata.rda")
